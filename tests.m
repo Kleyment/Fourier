230629130bd6e2f55766289da99a697f178748a0
Clement B <clement.bellanger3@etu.univ-lorraine.fr>
2019-03-12 00:04:14 +0100

    Calcul des distances et du contour

--- tests.m
+++ tests.m
@@ -1,5 +1,5 @@
 function [recall, precision] = my_tests()
-% calcul des descripteurs de Fourier de la base de donnÃ©es
+% calcul des descripteurs de Fourier de la base de donnÃƒÂ©es
 img_db_path = './db/';
 img_db_list = glob([img_db_path, '*.gif']);
 img_db = cell(1);
@@ -12,7 +12,7 @@ for im = 1:numel(img_db_list);
     [fd_db{im},~,~,~] = compute_fd(img_db{im});
 end
 
-% importation des images de requÃªte dans une liste
+% importation des images de requÃƒÂªte dans une liste
 img_path = './dbq/';
 img_list = glob([img_path, '*.gif']);
 t=tic()
@@ -30,14 +30,14 @@ for im = 1:numel(img_list)
     end
     [scores, I] = sort(scores);
        
-    % affichage des rÃ©sultats    
+    % affichage des rÃƒÂ©sultats    
     close all;
     figure(1);
-    top = 5; % taille du top-rank affichÃ©
+    top = 5; % taille du top-rank affichÃƒÂ©
     subplot(2,top,1);
     imshow(img); hold on;
     plot(m(1),m(2),'+b'); % affichage du barycentre
-    plot(poly(:,1),poly(:,2),'v-g','MarkerSize',1,'LineWidth',1); % affichage du contour calculÃ©
+    plot(poly(:,1),poly(:,2),'v-g','MarkerSize',1,'LineWidth',1); % affichage du contour calculÃƒÂ©
     subplot(2,top,2:top);
     plot(r); % affichage du profil de forme
     for i = 1:top
@@ -50,18 +50,69 @@ end
 end
 
 function [fd,r,m,poly] = compute_fd(img)
-N = 512; % Ã  modifier !!!
-M = 512; % Ã  modifier !!!
+% le nombre de points du contour
+N = 20; % A modifier !!!
+M = 512; % A modifier !!!
 h = size(img,1);
 w = size(img,2);
 [x,y,v]=find(img);
-m = [round(mean(y)) round(mean(x))]
-%m = [w/2 h/2]; % Ã  modifier !!!
-%m = [ 110 120];
+mx=round(mean(y));
+my=round(mean(x));
+m = [mx my];
 
-t = linspace(0,2*pi,100);
+% crée des angles N de 0 à 2 Pi avec le même espace
+t = linspace(0,2*pi,N);
 R = min(h,w)/2;
-poly = [m(1)+R*cos(t'), m(2)+R*sin(t')]; % Ã  modifier !!!
-r = R*ones(1,N); % Ã  modifier !!!
-fd = rand(1,M); % Ã  modifier !!!
+
+poly = zeros(N, 2);
+%r = R*ones(1,N); % ÃƒÂ  modifier !!!
+r = zeros(1,N);
+for i = 1:N
+
+j=1;
+% on cherche le point se situant dans la droite de l'angle au bord de
+% l'image
+% tant que j n'est pas au bord de l'image
+tmpX=mx;
+tmpY=my;
+while ((tmpX>1 & tmpY>1) & (tmpX<w & tmpY<h))
+   tmpX=round(mx+j*cos(t(1,i)));
+   tmpY=round(my+j*sin(t(1,i)));
+   j=j+1;
+end
+
+% le nombre d'itérations que l'on a fait pour sortir du cercle sera le même
+% que le nombre d'itérations pour aller du bord jusqu'au centre
+iterations=j;
+bordX=tmpX;
+bordY=tmpY;
+% on part du bord de l'image et on regarde le premier blanc avec lequel on
+% va calculer la distance au barycentre
+for j = 1:iterations
+   tmpX=round(bordX-j*cos(t(1,i)));
+   tmpY=round(bordY-j*sin(t(1,i)));
+   
+   % si le pixel est blanc on s'arrête et on sauvegarde tmpX et tmpY dans les
+   % contours de l'image
+   if (img(tmpY,tmpX) == 1)
+       dx=tmpX-mx;
+       dy=tmpY-my;
+       r(1,i)=sqrt(dx^2+dy^2);
+       poly(i,1)=tmpX;
+       poly(i,2)=tmpY;
+       break;
+   end
+end
+
+% si on n'a pas trouvé
+if (j == iterations)
+    dx=bordX-mx;
+    dy=bordY-my;
+    r(1,i)=sqrt(dx^2+dy^2);
+    poly(i,1)=bordX;
+    poly(i,2)=bordY;
+end
+
+end
+fd = rand(1,M); % ÃƒÂ  modifier !!!
 end
